eb8ff4d1a654aec87551c24394774ebb3e45e25b
khushi019 <khushikhandelwal419@gmail.com>
2025-08-07 03:44:41 +0530

    changes

--- Website/settings.py
+++ Website/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("ENGINE"),
+		'NAME': os.environ.get("NAME"),
+		'USER': os.environ.get("USER"),
+		'PASSWORD': os.environ.get("PASSWORD"),
+		'HOST':os.environ.get("HOST"),
+		'PORT':os.environ.get("PORT"),
     }
 }
 
